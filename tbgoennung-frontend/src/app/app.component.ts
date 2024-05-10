126590bed195cf0e1879615b90f7ccf210b37fac
maikloewen <loewenmaik@gmail.com>
2024-05-10 16:20:35 +0200

    Methode zum post erstellt. Eingabefeld wird noch nicht ausgelesen

--- tbgoennung-frontend/src/app/app.component.ts
+++ tbgoennung-frontend/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  niche: any;
+  constructor(private http: HttpClient) {}
+
+  postRequestAddNiche(niche: string) {
+    if (niche) {
+      this.http.post('http://127.0.0.1:5000/add_niche', { niche: niche }).subscribe(response => {
+        console.log(response);
+      });
+    }
+  }
+
   title = 'tbgoennung-frontend';
 }
--- tbgoennung-frontend/src/app/app.module.ts
+++ tbgoennung-frontend/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,10 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
