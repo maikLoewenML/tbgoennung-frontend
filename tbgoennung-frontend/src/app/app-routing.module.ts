803d1bb669c3ad17aac22ac7a6a17ae08aec53fb
maikloewen <loewenmaik@gmail.com>
2024-06-25 17:09:04 +0200

    Login Componente ausgebaut

--- tbgoennung-frontend/src/app/app-routing.module.ts
+++ tbgoennung-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TbgoennungComponent } from './tbgoennung/tbgoennung.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'tbgoennung', component: TbgoennungComponent }
+  { path: 'tbgoennung', component: TbgoennungComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
 ];
 
 @NgModule({
