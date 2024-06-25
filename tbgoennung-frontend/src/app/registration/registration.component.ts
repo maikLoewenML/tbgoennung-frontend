34998fab7a0e208fbd1ffa131e9279db56c61ba8
maikloewen <loewenmaik@gmail.com>
2024-06-25 17:46:14 +0200

    Registrierungskomponente bearbeitet

--- tbgoennung-frontend/src/app/registration/registration.component.ts
+++ tbgoennung-frontend/src/app/registration/registration.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
   email: string = '';
