903d547f77e6039d115abcec128ef266a8a707f8
maikloewen <loewenmaik@gmail.com>
2024-06-26 16:05:04 +0200

    Inhalt Registrierungskomponente eingebaut - Kommunikation mit dem Backend ermöglicht

--- tbgoennung-frontend/src/app/registration/registration.component.ts
+++ tbgoennung-frontend/src/app/registration/registration.component.ts
@@ -7,13 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  vorname: string = '';
+  name: string = '';
   email: string = '';
   password: string = '';
+  private backendUrl = 'http://127.0.0.1:5000'; 
 
   constructor(private http: HttpClient) {}
 
-  register() {
-    this.http.post('/register', { email: this.email, password: this.password }).subscribe(response => {
+  register(vorname: string, name: string, email: string, password: string) {
+    this.http.post(`${this.backendUrl}/register`, { vorname: this.vorname, name: this.name, email: this.email, password: this.password }).subscribe(response => {
       console.log('User registered', response);
     });
   }
